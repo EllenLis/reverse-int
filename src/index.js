927df5e329730208e0711303151b6af8ab7c966d
EllenLis <elena139@bk.ru>
2023-08-02 16:20:27 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+
+  while (n !== 0) {
+    let restNum = n % 10;
+    n = Math.floor(n / 10);
+    result = result * 10 + restNum;
+  }
+  return result;
 }
